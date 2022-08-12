a9dbcc835cf0a8225cf3a65c8e7e2c35a660bb4a
rifan.setiawan <rifan.setiawan@paper.id>
2022-08-12 15:45:29 +0700

    first init commit

---
+++ MyNoteTrainer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyNoteTrainer
+//
+//  Created by Muhammad Rifan Setiawan on 12/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyNoteTrainer/MyNoteTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNoteTrainerApp.swift
+//  MyNoteTrainer
+//
+//  Created by Muhammad Rifan Setiawan on 12/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MyNoteTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
