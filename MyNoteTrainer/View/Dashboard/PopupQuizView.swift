840181dcb6ea961d0bbb13698d06fad9605bc38c
aldialfian19 <105551380+aldialfian19@users.noreply.github.com>
2022-08-19 09:18:34 +0800

    dashpage

---
+++ MyNoteTrainer/View/Dashboard/PopupQuizView.swift
@@ -0,0 +1,94 @@
+//
+//  PopupView.swift
+//  MyNoteTrainer
+//
+//  Created by Rinaldi Alfian on 19/08/22.
+//
+
+import SwiftUI
+
+struct PopupQuizView: View {
+    var body: some View {
+        VStack{
+            Image("PopupBack")
+            HStack {
+                Text("Choose Quiz")
+                    .font(.system(size: 18, weight: .semibold))
+                Spacer()
+            }.padding()
+            
+            Button(action: {}, label: {
+                HStack{
+                    Image("Popup1")
+                        .frame(width: 97, height: 59)
+                    VStack(alignment: .leading){
+                        Text("Musical Note")
+                            .foregroundColor(Color.black)
+                        
+                        HStack{
+                            Circle()
+                                .frame(width: 10, height: 10)
+                                .foregroundColor(Color.green)
+                            Text("Beginner")
+                                .font(.system(size: 12, weight: .regular))
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                    Spacer()
+                }
+                .padding()
+            })
+            
+            Button(action: {}, label: {
+                HStack{
+                    Image("Popup2")
+                    
+                    VStack(alignment: .leading){
+                        Text("Clef")
+                            .foregroundColor(Color.black)
+                        
+                        HStack{
+                            Circle()
+                                .frame(width: 10, height: 10)
+                                .foregroundColor(Color.green)
+                            Text("Beginner")
+                                .font(.system(size: 12, weight: .regular))
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                    Spacer()
+                }
+                .padding()
+            })
+            
+            Button(action: {}, label: {
+                HStack{
+                    Image("Popup3")
+                        .frame(width: 97, height: 59)
+                    VStack(alignment: .leading){
+                        Text("Music Scale")
+                            .foregroundColor(Color.black)
+                        
+                        HStack{
+                            Circle()
+                                .frame(width: 10, height: 10)
+                                .foregroundColor(Color.brownColor)
+                            Text("Intermediate")
+                                .font(.system(size: 12, weight: .regular))
+                                .foregroundColor(Color.gray)
+                        }
+                    }
+                    Spacer()
+                }
+                .padding()
+            })
+           
+        }
+    }
+}
+
+struct PopupQuizView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopupQuizView()
+    }
+}
---
+++ MyNoteTrainer/View/Practice/PracticeView.swift
@@ -0,0 +1,20 @@
+//
+//  PracticeView.swift
+//  MyNoteTrainer
+//
+//  Created by Rinaldi Alfian on 18/08/22.
+//
+
+import SwiftUI
+
+struct PracticeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PracticeView_Previews: PreviewProvider {
+    static var previews: some View {
+        PracticeView()
+    }
+}
